cc3830c00c38c2ce635416555e7c6c8e3ad39248
anthonychaussin <chaussin.anthony@gmail.com>
2019-09-25 12:14:24 +0200

    Erreur 500

--- API/index.php
+++ API/index.php
@@ -11,13 +11,13 @@ $DataBase_Name = "iot";
 $User_Id = "admin";
 $PWD = "admin";
 
-/*
+
 
 $db = new PDO("$sgbd:server=$Server_Name;Database=$DataBase_Name", $User_Id, $PWD);
 $Query_Values = 'SELECT date_value AS [Date], temp_value AS [Temperature], humid_value AS [Pourcentage_Humidite] FROM value';
 
 $Result = $db->query($Query_Values);
-
+/*
 $Result->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Value');
 $Tab_Value = $Result->fetch();
 
